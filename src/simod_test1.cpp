67a29bea3d23fb8ba73e42806975e4130cf5cdc8
Italo-99 <italoalmirante.10@gmail.com>
2025-01-20 19:33:31 +0100

    Simod test double manipulator completed

--- src/simod_test1.cpp
+++ src/simod_test1.cpp
@@ -5,12 +5,13 @@
 int main(int argc, char** argv) {
 
     // Initialize the ROS node
-    ros::init(argc, argv, "manipulator_sequence_publisher");
+    ros::init(argc, argv, "simod_test1");
     ros::NodeHandle nh;
 
     // Publishers
+    // ros::Publisher pose_pub = nh.advertise<geometry_msgs::Pose>("/manipulator/desired_tcp_pose",  1);
     ros::Publisher pose_pub = nh.advertise<geometry_msgs::Pose>("/manipulator/adm_xd",  1);
-    ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("/manipulator/tcp_vel", 1);
+    ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("/manipulator/cmd_vel", 1);
 
     // Define the pose message
     geometry_msgs::Pose pose_msg;
@@ -23,11 +24,17 @@ int main(int argc, char** argv) {
     pose_msg.orientation.w = 0.707;
 
     // Publish the pose message
-    ROS_INFO("Publishing pose to /manipulator/adm_xd");
-    pose_pub.publish(pose_msg);
+    ROS_INFO("Wait for the other robot");
+    ros::Rate loop_rate(500); // 10 Hz
+    ros::Time start_time = ros::Time::now();
+
+    while (ros::Time::now() - start_time < ros::Duration(4.0)) {
+        pose_pub.publish(pose_msg);
+        loop_rate.sleep();
+    }
 
     // Wait for 5 seconds
-    ros::Duration(5.0).sleep();
+    ros::Duration(1.0).sleep();
 
     // Define the velocity message
     geometry_msgs::Twist vel_msg;
@@ -38,22 +45,53 @@ int main(int argc, char** argv) {
     vel_msg.angular.y = 0.0;
     vel_msg.angular.z = 0.0;
 
-    ROS_INFO("Publishing velocity to /manipulator/tcp_vel");
-    ros::Time start_time = ros::Time::now();
+    ROS_INFO("Publishing velocity to /manipulator/cmd_vel");
+    start_time = ros::Time::now();
 
     // Publish the velocity for 6 seconds
-    ros::Rate loop_rate(10); // 10 Hz
-    while (ros::Time::now() - start_time < ros::Duration(6.0)) {
+    while (ros::Time::now() - start_time < ros::Duration(4.0)) {
         vel_pub.publish(vel_msg);
         loop_rate.sleep();
     }
+
+    start_time = ros::Time::now();
 
     // Stop the manipulator by setting velocity to 0
     vel_msg.linear.z = 0.0;
     ROS_INFO("Stopping manipulator");
     vel_pub.publish(vel_msg);
 
-    ros::Duration(0.1).sleep(); // Give some time to ensure the stop message is published
+    while (ros::Time::now() - start_time < ros::Duration(2.0)) {
+        vel_pub.publish(vel_msg);
+        loop_rate.sleep();
+    }
+
+    start_time = ros::Time::now();
+
+    vel_msg.linear.z = -0.10;
+    ROS_INFO("Going down");
+    // Publish the velocity for 6 seconds
+    while (ros::Time::now() - start_time < ros::Duration(4.0)) {
+        vel_pub.publish(vel_msg);
+        loop_rate.sleep();
+    }
+
+
+    // Stop the manipulator by setting velocity to 0
+    vel_msg.linear.z = 0.0;
+    ROS_INFO("Stopping manipulator");
+    vel_pub.publish(vel_msg);
+
+    start_time = ros::Time::now();
+
+    // Publish the velocity for 6 seconds
+    while (ros::Time::now() - start_time < ros::Duration(1.0)) {
+        vel_pub.publish(vel_msg);
+        loop_rate.sleep();
+    }
+
+    ros::Duration(1).sleep(); // Give some time to ensure the stop message is published
+
 
     ROS_INFO("Sequence completed");
     return 0;
